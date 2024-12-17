95e38f643b550af442891a8a1780c348bfbc067a
community-vercel <zuhooruddin055#gmail.com>
2024-12-17 04:41:35 -0800

    commited

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,16 +1,40 @@
+'use client';
+
 import Sidebar from './Side';
 import Header from './Header';
-
+import {useState,useEffect} from 'react'
+import { useRouter } from 'next/navigation';
+import Login from '../login/page';
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const [user,setUser]=useState()
+
+  useEffect(() => {
+      // Check if user data is available in localStorage (this indicates login)
+      const user = localStorage.getItem('superAdmin');
+  
+      if (!user) {
+        // If no user data is found, redirect to the login page
+        router.push('/login');
+      }
+      else{
+        setUser(user)
+      }
+    }, [router]);
   return (
-    <div className="flex">
+    <>
+    {user && user?(
+      <div className="flex">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 p-6">{children}</main>
       </div>
     </div>
-  );
+    
+    ):''}
+  </>
+  )
 };
 
 export default Layout;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client';
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Dashboard from './components/Dashboard';
 import { ToastContainer } from 'react-toastify';
+import Login from './login/page';
 
 export default function Home() {
   const router = useRouter();
+  const [user,setUser]=useState()
 
   useEffect(() => {
     // Check if user data is available in localStorage (this indicates login)
@@ -15,12 +17,20 @@ export default function Home() {
       // If no user data is found, redirect to the login page
       router.push('/login');
     }
+    else{
+      setUser(user)
+    }
   }, [router]);
 
   // If the user is logged in, show the Dashboard
   return( 
 <>
-<Dashboard />;
+{user && user?(
+  <Dashboard />
+
+):(
+<Login />
+)}
 <ToastContainer /> {/* This will display your toasts */}
 
 </>
